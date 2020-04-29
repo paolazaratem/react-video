df520766eae23cb32a0acdddcfbc0553be5b2459
paolazaratem <paola.zarate@gmail.com>
2020-04-29 15:45:08 -0500

    Add Button2 component
    Paola Zarate- Geek girl

--- src/components/Button2.jsx
+++ src/components/Button2.jsx
@@ -1,22 +1,45 @@
 import React from "react";
 
 class Button2 extends React.Component {
+  // const [ state, setState ] = useState(0);
+
   state = {
     count: 0,
   };
 
-  handleClick = () => {
+  handleClickAdd = () => {
     this.setState({
       count: this.state.count + 1,
     });
   };
+
+  handleClickRemove = () => {
+    if (this.state.count > 0) {
+      this.setState({
+        count: this.state.count - 1,
+      });
+    }
+  };
+
+  handleClickReset = () => {
+    this.setState({
+      count: 0,
+    });
+  };
+
   render() {
     const { count } = this.state;
     return (
       <div>
         <h1>Apples: {count} </h1>
-        <button type="button" onClick={this.handleClick}>
+        <button type="button" onClick={this.handleClickAdd}>
           Add
+        </button>
+        <button type="button" onClick={this.handleClickRemove}>
+          Remove
+        </button>
+        <button type="button" onClick={this.handleClickReset}>
+          Reset
         </button>
       </div>
     );
